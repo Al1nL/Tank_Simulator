b57b0c8f16f6b3fc354648154ef6e8ee33f339d0
Al1nL <alinl@mail.tau.ac.il>
2025-06-27 13:47:23 +0300

    add user common base files

---
+++ UserCommon/header/Mine.h
@@ -0,0 +1,26 @@
+#ifndef MINE_H
+#define MINE_H
+#include "GameObject.h"
+using std::pair;
+
+class Mine : public GameObject
+{
+private:
+    bool triggered = false;
+    bool destroyed = false;
+    static const char MINE_SYMBOL = '@'; // Assuming this is the intended symbol
+
+public:
+    Mine(pair<int, int> pos);
+    ~Mine() override = default;
+
+    Mine(Mine const &) = delete;
+    Mine &operator=(const Mine &) = delete;
+
+    void trigger();
+    bool isTriggered() const;
+    void destroy() override;
+    bool isDestroyed() const override;
+    char getSymbol() const override;
+};
+#endif // MINE_H
---
+++ UserCommon/header/Shell.h
@@ -0,0 +1,34 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include "GameObject.h"
+using std::pair;
+
+class Shell : public GameObject
+{
+    Direction direction;
+    int ownerId;
+    bool destroyed;
+    static const char SHELL_SYMBOL = '*';
+
+public:
+    // Constructor
+    Shell(pair<int, int> pos, Direction dir, int owner);
+    ~Shell() override = default;
+
+    Shell(Shell const &) = delete;
+    Shell &operator=(const Shell &) = delete;
+
+    // Accessors
+    Direction getDirection() const;
+    void setDirection(Direction dir);
+
+    int getOwnerId() const;
+    bool isDestroyed() const override;
+
+    // Mutators
+    void destroy() override;
+
+    // Symbol for representation on the board
+    char getSymbol() const override;
+};
+#endif // SHELL_H
