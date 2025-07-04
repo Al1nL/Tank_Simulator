ffbdf0213f2570afeb52e785f604ec06b1e8a4f1
Al1nL <alinl@mail.tau.ac.il>
2025-07-04 18:38:06 +0300

    add algo base files for better usage later

--- Algorithm/header/TankAlgorithm_212535058_324022904.h
+++ Algorithm/header/TankAlgorithm_212535058_324022904.h
@@ -1,9 +1,9 @@
 #ifndef TANKALGORITHM_212535058_324022904_H
 #define TANKALGORITHM_212535058_324022904_H
 
-#include "../../common/TankAlgorithm.h"
-#include "../../common/ActionRequest.h"
-#include "../../UserCommon/header/GameObject.h"
+
+#include "MyTankAlgorithm.h"
+
 #include "TankBattleInfo.h"
 
 #include <memory>
@@ -23,34 +23,9 @@ namespace Algorithm_212535058_324022904 {
         Direction newDir;
     };
 
-    class TankAlgorithm_212535058_324022904 : public TankAlgorithm {
-        int player_index;
-        int tank_index;
+    class TankAlgorithm_212535058_324022904 : public MyTankAlgorithm {
         int current_turn = 0;
         int last_info_update = 0;
-
-        unique_ptr<TankBattleInfo> battle_info;
-        pair<int, int> nextStep(bool forward, const pair<int, int> pos, const Direction dir);
-        ActionRequest determineRotation(Direction currentDir, Direction desiredDir);
-
-        // Safety check for backward movement
-        bool canSafelyBack(int backR, int backC);
-        bool willBeHitIn(int row, int col, int t);
-        bool isAlignedWithOpponent(pair<int, int> opponentPos);
-        ActionRequest checkForEscape();
-
-        // decision utilities
-        bool isOccupierFree(pair<int, int> pos);
-        bool shouldShootOpponent(const pair<int, int> &opponentPos);
-        bool canMoveFwd();
-        bool canMoveBack();
-        void rotate(ActionRequest action);
-        bool isValidMove(ActionRequest action);
-        Direction calculateDirection(int currRow, int currCol, int targetRow, int targetCol);
-        OppData getClosestOpponent();
-        int calculateActionsToOpponent(const OppData &oppPos);
-        void updateInnerInfoAfterAction(ActionRequest action);
-        int wrap(int value, int size) { return size != -1 ? (value % size + size) % size : value; }; // wrap-around edges
 
         vector<ActionRequest> rotations = {ActionRequest::RotateLeft45, ActionRequest::RotateLeft90, ActionRequest::RotateRight45, ActionRequest::RotateRight90};
         int countOpenSpaceInDirection(pair<int, int> pos);
@@ -59,7 +34,7 @@ namespace Algorithm_212535058_324022904 {
         bool canShootAfterRotate(Direction targetDir, OppData &opp);
         bool shouldShootOpponent(OppData &opp);
         Direction simulateRotation(ActionRequest act);
-        ActionRequest decideAction();
+        ActionRequest decideAction() override;
         void moveKnownShells();
 
     public:
@@ -70,10 +45,6 @@ namespace Algorithm_212535058_324022904 {
         TankAlgorithm_212535058_324022904 &operator=(const TankAlgorithm_212535058_324022904 &) = delete;
 
         ActionRequest getAction() override;
-        void updateBattleInfo(BattleInfo&) override;
-
-        int getTankId() const { return tank_index; }
-        int getOwnerId() const { return player_index; }
     };
 
 }
