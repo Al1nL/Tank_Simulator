b79a5b8853375043b5cf485c7de5875be0ae3909
alinl <alinl@mail.tau.ac.il>
2025-08-15 15:39:26 +0000

    fix game manager

--- Simulator/header/ThreadPool.h
+++ Simulator/header/ThreadPool.h
@@ -34,13 +34,23 @@ public:
     }
 
     template<class F, class... Args>
-    void enqueue(F&& f, Args&&... args) {
-        auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
+        using return_type = typename std::result_of<F(Args...)>::type;
+        
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        
+        std::future<return_type> res = task->get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            tasks.emplace([task]() { task(); });
+            if(stop) {
+                throw std::runtime_error("enqueue on stopped ThreadPool");
+            }
+            tasks.emplace([task](){ (*task)(); });
         }
         condition.notify_one();
+        return res;
     }
 
     void waitAll() {
